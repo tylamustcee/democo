ad8ce897cde9e18fd3fb94469e16bddb00dad407
tylamustcee <tyla@mustcee.com.au>
2026-05-07 14:20:24 +1000

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,4 +1,3 @@
-```javascript
 const client_id = process.env.GITHUB_CLIENT_ID;
 const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
@@ -9,12 +8,12 @@ exports.handler = async (event) => {
     const params = new URLSearchParams({
       client_id,
       scope: "repo,user",
-      redirect_uri: `${process.env.URL}/.netlify/functions/auth`,
+      redirect_uri: process.env.URL + "/.netlify/functions/auth",
     });
     return {
       statusCode: 302,
       headers: {
-        Location: `https://github.com/login/oauth/authorize?${params}`,
+        Location: "https://github.com/login/oauth/authorize?" + params,
       },
     };
   }
@@ -34,37 +33,23 @@ exports.handler = async (event) => {
     if (data.error) {
       return {
         statusCode: 400,
-        body: `Error: ${data.error_description}`,
+        body: "Error: " + data.error_description,
       };
     }
 
     const token = data.access_token;
+    const payload = JSON.stringify({ token: token, provider: "github" });
+    const message = "authorization:github:success:" + payload;
 
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/html" },
-      body: `<!DOCTYPE html>
-<html>
-<body>
-<script>
-  (function() {
-    var token = ${JSON.stringify(token)};
-    var message = "authorization:github:success:" + JSON.stringify({token: token, provider: "github"});
-    if (window.opener) {
-      window.opener.postMessage(message, "*");
-    }
-    window.close();
-  })();
-</script>
-<p>Authorizing... you can close this window.</p>
-</body>
-</html>`,
+      body: "<!DOCTYPE html><html><body><script>(function(){var m=" + JSON.stringify(message) + ";if(window.opener){window.opener.postMessage(m,'*');}window.close();})();<\/script><p>Authorizing...</p></body></html>",
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: `Server error: ${err.message}`,
+      body: "Server error: " + err.message,
     };
   }
 };
-```
